0e17f0e090dc912e6cca77ec5878aa3c8e7b0c61
Andrew D. Yates <andrewyates.name@gmail.com>
2012-08-26 18:55:50 -0400

    working batch_minepy_pairwise (no dispatcher yet)

--- batch_minepy_pairwise.py
+++ batch_minepy_pairwise.py
@@ -7,20 +7,23 @@ Computes (and saves matrices for):
 
 Unlike other batch_pairwise.py script, this computes several matrices at once; one for
   each of the MINE statistics.
+
+EXAMPLE MANUAL USE:
+  time python $HOME/dependency_matrix_dispatcher/batch_minepy_pairwise.py npyfile=/fs/lustre/osu6683/GSE7307.normed.tab.pkl work_dir=/fs/lustre/osu6683/gse7307/minepy_test n=54675 start=0 end=10 batchname=minejar_test
 """
 from util import *
 import numpy as np
+import cPickle as pickle
 import numpy.ma as ma
 from py_symmetric_matrix import *
 import sys
 # minepy must be installed. See http://mpba.fbk.eu/cmine
 import minepy
 
-#BATCH_CMD = "time python %(script_path)s/batch_minepy_pairwise.py npyfile=%(npyfile)s offset=%(offset)d k=%(k)d work_dir=%(work_dir)s function=%(function)s n=%(n)d >> %(stdout_fname)s 2>> %(stderr_fname)s"
-
 REPORT_N = 50000
 
 def main(npyfile=None, work_dir=None, n=None, start=None, end=None, batchname=None, alpha=0.6, c=15):
+  # note: n could be computed from `npyfile` contents
   function = "minepy"
   assert npyfile, work_dir
   n, start, end = map(int, (n, start, end))
@@ -49,10 +52,15 @@ def main(npyfile=None, work_dir=None, n=None, start=None, end=None, batchname=No
     end = int(end)
 
   print "Loading %s..." % (npyfile)
-  # TODO: load level 2 pickle rather than numpy's inefficient MaskedArray ASCII binary format.
-  M = ma.load(npyfile)
+  # load level 2 pickle rather than numpy's inefficient MaskedArray ASCII binary format if .pkl
+  if npyfile.rpartition('.')[2].lower() == 'pkl':
+    print "Loading as level 2 pickle"
+    M = pickle.load(open(npyfile))
+  else:
+    print "Loading as level 0 numpy.MaskedArray pickle"
+    M = ma.load(npyfile)
   MIC, MAS, MEV, MCN = np.zeros(end-start), np.zeros(end-start), np.zeros(end-start), np.zeros(end-start)
-  n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcv = 0, 0, 0, 0
+  n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcn = 0, 0, 0, 0
   print "Starting to write %d pairs for %s" % (end-start, batchname)
   for i, j in enumerate(xrange(start, end)):
     if i % REPORT_N == 0:
@@ -67,19 +75,19 @@ def main(npyfile=None, work_dir=None, n=None, start=None, end=None, batchname=No
     except IndexError:
       print "WARNING! INDEX ERROR! i %d, x %d, y %d, n %d" %(i,x,y,n)
       raise
-    MIC[i], MAS[i], MEV[i], MCV[i] = mine.mic(), mine.mas(), mine.mev(), mine.mcv()
+    MIC[i], MAS[i], MEV[i], MCN[i] = mine.mic(), mine.mas(), mine.mev(), mine.mcn()
     if np.isnan(MIC[i]):
       n_nan_mic += 1
     if np.isnan(MAS[i]):
       n_nan_mas += 1
     if np.isnan(MEV[i]):
       n_nan_mev += 1
-    if np.isnan(MCV[i]):
-      n_nan_mcv += 1
+    if np.isnan(MCN[i]):
+      n_nan_mcn += 1
 
   print "Computed %d pairs for %s" % (end-start, batchname)
-  print "%d mic nans, %d mas nans, %d mev nans, %d mcv nans" % (n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcv)
-  n_bad = sum((n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcv))
+  print "%d mic nans, %d mas nans, %d mev nans, %d mcn nans" % (n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcn)
+  n_bad = sum((n_nan_mic, n_nan_mas, n_nan_mev, n_nan_mcn))
   if n_bad > 0:
     print "!!!WARNING: There exists at least one (%d) not-a-numbers (nans) in this batch." % n_bad
 
@@ -99,9 +107,9 @@ def main(npyfile=None, work_dir=None, n=None, start=None, end=None, batchname=No
   np.save(output_fname, MEV)
   print "Saved %s." % output_fname
 
-  output_fname = os.path.join(work_dir, batchname+".mcv.npy")
-  print "Saving MCV results %d through %d as %s. (zero-indexed)" % (start, end-1, output_fname)
-  np.save(output_fname, MCV)
+  output_fname = os.path.join(work_dir, batchname+".mcn.npy")
+  print "Saving MCN results %d through %d as %s. (zero-indexed)" % (start, end-1, output_fname)
+  np.save(output_fname, MCN)
   print "Saved %s." % output_fname
 
   
