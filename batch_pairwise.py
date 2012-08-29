e01448eabfcb53315f044564a79236cfd23541ec
Andrew Yates <andrewyates.name@gmail.com>
2012-08-29 18:09:36 -0400

    revised batch_pairwise

--- batch_pairwise.py
+++ batch_pairwise.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""Compute batch of pairwise dependencies.
+
+EXAMPLE MANUAL USE:
+  time python $HOME/dependency_matrix_dispatcher/batch_pairwise.py function=dcor npyfile=/fs/lustre/osu6683/GSE7307.normed.tab.pkl work_dir=/fs/lustre/osu6683/gse7307/hhgR_test n=54675 start=0 end=10 batchname=hhgR_test verbose=True
+"""
 from util import *
 import numpy.ma as ma
 from py_symmetric_matrix import *
@@ -9,22 +14,10 @@ import cPickle as pickle
 
 REPORT_N = 50000
 
-def main(npyfile=None, work_dir=None, function=None, n=None, start=None, end=None, batchname=None):
+def main(npyfile=None, work_dir=None, function=None, n=None, start=None, end=None, batchname=None, verbose=False, *args, **kwds):
   
-  assert npyfile, work_dir
+  assert npyfile and work_dir
   assert function in FUNCTIONS
-  n, start, end = map(int, (n, start, end))
-  assert n > 0 and start >= 0 and end > 0
-  if batchname is None or batchname in ("None", "NONE", "none"):
-    batchname = "%s_%s_%d_%d" % \
-      (os.path.basename(npyfile), function, start, end)
-
-  # Do not recreate existing files.
-  output_fname = os.path.join(work_dir, batchname+".npy")
-  if os.path.exists(output_fname):
-    print "%s already exists. Exiting..." % output_fname
-    return 1
-
   if start is None:
     start = 0
   else:
@@ -33,7 +26,20 @@ def main(npyfile=None, work_dir=None, function=None, n=None, start=None, end=Non
     end = n*(n-1) / 2
   else:
     end = int(end)
+  n = int(n)
+  assert n > 0 and start >= 0 and end > 0
 
+  # If batchname not provided, compute default value. Use batchname in output file name.
+  if batchname is None or batchname.lower() in ("false", "f", "none"):
+    batchname = "%s_%s_%d_%d" % \
+      (os.path.basename(npyfile), function, start, end)
+
+  # Do not recreate existing batch output files.
+  output_fname = os.path.join(work_dir, batchname+".npy")
+  if os.path.exists(output_fname):
+    print "%s already exists. Exiting..." % output_fname
+    return 1
+  
   # Load data file
   print "Loading %s..." % npyfile
   if npyfile.rpartition('.')[2].lower() == 'pkl':
@@ -42,13 +48,14 @@ def main(npyfile=None, work_dir=None, function=None, n=None, start=None, end=Non
   else:
     print "Loading as level 0 numpy.MaskedArray pickle"
     M = ma.load(npyfile)
-  
+
+  # TODO: Revise this as some kind of factory selector.
   f = FUNCTIONS[function]
   if function == "dcor":
     print "dCOR implementation:", DCOR_LIB
-    
+
+  # Compute
   R = np.zeros(end-start)
-  n_nan = 0
   print "Starting to write %d pairs for %s" % (end-start, batchname)
   for i, j in enumerate(xrange(start, end)):
     if i % REPORT_N == 0:
@@ -61,17 +68,21 @@ def main(npyfile=None, work_dir=None, function=None, n=None, start=None, end=Non
 
     try:
       shared_mask = ~(M[x].mask | M[y].mask)
-      R[i] = f(M[x][shared_mask].data, M[y][shared_mask].data)
     except IndexError:
       print "WARNING! INDEX ERROR! i %d, x %d, y %d, n %d" %(i,x,y,n)
       raise
-    if np.isnan(R[i]):
-      n_nan += 1
+    X, Y = M[x][shared_mask].data, M[y][shared_mask].data
+    assert np.size(X) == np.size(Y) <= np.size(M,1)
+    R[i] = f(X,Y)
+    if verbose:
+      print i, j, x, y, R[i]
 
+  assert end-start == np.size(R)
+  n_nan = np.sum(np.isnan(R))
   print "Computed %d pairs for %s" % (end-start, batchname)
   print "%d nans" % (n_nan)
-  if n_nans > 0:
-    print "!!!WARNING: There exists at least one (%d) not-a-numbers (nans) in this batch." % n_nans
+  if n_nan > 0:
+    print "!!!WARNING: There exists at least one (%d) not-a-numbers (nans) in this batch." % (n_nan)
 
   output_fname = os.path.join(work_dir, batchname+".npy")
   print "Saving results %d through %d as %s. (zero-indexed)" % (start, end-1, output_fname)
--- util.py
+++ util.py
@@ -4,8 +4,8 @@ import subprocess
 import datetime
 import cPickle as pickle
 # Comment cpy dcor, uncomment native python dcor for pychecker.
-from dcor_cpy import *
-#from dcor import *
+#from dcor_cpy import *
+from dcor import *
 import os
 import errno
 from numpy import ma
